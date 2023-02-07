5645f274615fc3bd34ce248896e38536b211f9dc
AbaniwoM <michaelabaniwo@gmail.com>
2023-02-07 02:48:26 +0100

    build sociomorre app

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -172,3 +172,99 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// import React, { useState } from 'react';
+// import { Box } from '@mui/system';
+// import FlexBetween from "components/FlexBetween";
+// import { Typography, useMediaQuery, InputBase, IconButton, useTheme, FormControl, Select, MenuItem } from '@mui/material';
+// import { Search, DarkMode, LightMode, Message, Notifications, Help, Menu} from "@mui/icons-material";
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setMode, setLogout } from "state";
+
+// const Navbar = () => {
+//   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const user = useSelector((state) => state.user);
+//   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+//   const theme = useTheme();
+//   const neutralLight = theme.palette.neutral.light;
+//   const primaryLight = theme.palette.primary.light;
+//   const dark = theme.palette.neutral.dark;
+//   const alt = theme.palette.background.alt;
+
+//   // const fullName = `${user.firstName} ${user.lastName}`;
+//   return (
+//     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+//       <FlexBetween gap="1.75rem">
+//         <Typography
+//         fontWeight="bold" 
+//         fontSize="clamp(1rem, 2rem, 2.25rem)"
+//         color="primary"
+//         onClick={() => navigate("/home")}
+//         sx={{
+//           "&:hover": {
+//               color: primaryLight,
+//               cursor: "pointer",
+//           },
+//         }}>
+//           SocioMorre
+//         </Typography>
+//         {isNonMobileScreens && (
+//             <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+//                <InputBase placeholder="Search..."/>
+//                <IconButton>
+//                 <Search />
+//                </IconButton>
+//             </FlexBetween>
+//         )}
+//       </FlexBetween>
+
+//       {/* DESKTOP NAV */}
+//        {isNonMobileScreens ? (
+//          <FlexBetween gap="2rem">
+//            <IconButton onClick={() => dispatch(setMode())}>
+//              {theme.palette.mode === "dark" ? (
+//                  <DarkMode sx={{ fontSize: "25px" }} />
+//              ) : (
+//                  <LightMode sx={{ color: dark, fontSize: "25px" }} />
+//              )}
+//            </IconButton>
+//            <Message sx={{ fontSize: "25px" }} />
+//            <Notifications sx={{ fontSize: "25px" }} />
+//            <Help sx={{ fontSize: "25px" }} />
+//            <FormControl variant="standard" value="Michael">
+//              <Select
+//                  sx={{
+//                      backgroundColor: neutralLight,
+//                      width: "150px",
+//                      borderRadius: "0.25rem",
+//                      p: "0.25rem 1rem",
+//                      "& .MuiSvgIcon-root": {
+//                          pr: "0.25rem",
+//                          width: "3rem"
+//                      },
+//                      "& .MuiSelect-select:focus": {
+//                          backgroundColor: neutralLight
+//                      }
+//                  }}
+//                  input={<InputBase />}
+//              >
+//                <MenuItem>
+//                  <Typography>Michael</Typography>
+//                </MenuItem>
+//                <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+//              </Select>
+//            </FormControl> 
+//          </FlexBetween>
+//        ): (
+//          <IconButton onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}>
+//            <Menu />
+//          </IconButton>
+//        )}
+//     </FlexBetween>
+//   )
+// }
+
+// export default Navbar
